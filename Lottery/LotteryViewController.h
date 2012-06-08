4ed6828947754a288bb28e1bc350bd0d127c3cec
jetspeed <shmimy-w@163.com>
2012-06-08 22:21:04 +0800

    add view and shake

--- Lottery/LotteryViewController.h
+++ Lottery/LotteryViewController.h
@@ -8,6 +8,22 @@
 
 #import <UIKit/UIKit.h>
 
-@interface LotteryViewController : UIViewController
+@interface LotteryViewController : UIViewController {
+    IBOutlet UILabel *ball1;
+    IBOutlet UILabel *ball2;
+    IBOutlet UILabel *ball3;
+    IBOutlet UILabel *ball4;
+    IBOutlet UILabel *ball5;
+    IBOutlet UILabel *ball6;
+}
+
+
+@property (nonatomic, retain)  IBOutlet UILabel *ball1;
+@property (nonatomic, retain)  IBOutlet UILabel *ball2;
+@property (nonatomic, retain)  IBOutlet UILabel *ball3;
+@property (nonatomic, retain)  IBOutlet UILabel *ball4;
+@property (nonatomic, retain)  IBOutlet UILabel *ball5;
+@property (nonatomic, retain)  IBOutlet UILabel *ball6;
+
 
 @end
--- Lottery/LotteryViewController.m
+++ Lottery/LotteryViewController.m
@@ -9,11 +9,26 @@
 #import "LotteryViewController.h"
 
 @implementation LotteryViewController
+@synthesize ball1;
+@synthesize ball2;
+@synthesize ball3;
+@synthesize ball4;
+@synthesize ball5;
+@synthesize ball6;
+
 
 - (void)didReceiveMemoryWarning
 {
     [super didReceiveMemoryWarning];
     // Release any cached data, images, etc that aren't in use.
+}
+
+- (void)motionEnded:(UIEventSubtype)motion withEvent:(UIEvent *)event
+{
+    if (motion == UIEventSubtypeMotionShake)
+    {
+        [ball1 setText:@"11"];
+    }
 }
 
 #pragma mark - View lifecycle
@@ -33,22 +48,29 @@
 
 - (void)viewWillAppear:(BOOL)animated
 {
+    
     [super viewWillAppear:animated];
 }
 
 - (void)viewDidAppear:(BOOL)animated
 {
     [super viewDidAppear:animated];
+    [self becomeFirstResponder];
 }
 
 - (void)viewWillDisappear:(BOOL)animated
 {
+    [self resignFirstResponder];
 	[super viewWillDisappear:animated];
 }
 
 - (void)viewDidDisappear:(BOOL)animated
 {
 	[super viewDidDisappear:animated];
+}
+
+-(BOOL)canBecomeFirstResponder {
+    return YES;
 }
 
 - (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
